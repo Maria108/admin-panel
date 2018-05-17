dd1d01017f29b07ce5e7fa535ca19c5ed978916c
Maria108 <maria.abash@gmail.com>
2018-05-17 10:22:43 -0400

    LOgin, signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   resources :masters
   resources :courses
   resources :cohorts
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
