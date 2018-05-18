5bf1990a54e1bf37f7d43cf0f92b78a15a1fc069
Maria108 <maria.abash@gmail.com>
2018-05-18 17:56:56 -0400

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  devise_for :users
   resources :users
   resources :students
   resources :masters
   resources :courses
   resources :cohorts
 
+  get 'welcome/index'
   root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
