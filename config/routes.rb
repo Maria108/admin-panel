396ae734640d3f698c0baa905015d99b6d867607
Maria108 <maria.abash@gmail.com>
2018-05-18 17:26:10 -0400

    Adds welcome controller, installs devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :users
   resources :students
   resources :masters
   resources :courses
   resources :cohorts
+
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
