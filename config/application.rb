cb92b2e84a1e79352dcd8619b99c7a024b96a010
Maria108 <maria.abash@gmail.com>
2018-05-24 17:56:43 -0400

    Fixes errors

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module MarvelLyceum
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
+    config.filter_parameters << :password
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -20,6 +23,5 @@ end
 
 Raven.configure do |config|
   config.dsn = 'https://dbdb66367936406ab6accf0c06097467:ba1d402342b14ed49b66627b6870d2f0@sentry.io/1213120'
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
-
-config.filter_parameters << :password
